e11981368737ffce508c219b1dd316824f3aadbd
Alex Norris <Alexanderdahlawesome@gmail.com>
2017-05-09 09:08:33 +0200

    Update

---
+++ Projekt/Beregninger/Kabinet.m
@@ -0,0 +1,18 @@
+%% LYAK Projekt - Beregninger af Højtalerkabinet
+
+%% Parametre
+Re=0;                               %DC-Modstand
+Le=0;                               %Svingspolens Selvinduktion
+fs=0;                               %Resonansfrekvens
+Mms=0;                              %Masse af Bevægelige system
+Cms=0;                              %Eftergivelighed af styr
+Qms=0;                              %Mekanisk Godhed
+Qes=0;                              %Elektrisk Godhed
+Rms=0;                              %Mekanisk tabsmodstand
+Vas=0;                              %Ækvivalent Volumen
+Bl=0;                               %Kraftfaktor
+SD=0;                               %Membranens effektive Areal
+Xmax=0;                             %Maksimal Lineær Bevægelse
+S=0;                                %Følsomhed
+
+
