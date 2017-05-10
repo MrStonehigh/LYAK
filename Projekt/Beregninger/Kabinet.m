29ba49637a3331ddcc71eaf7f4c59824e5a3a7e6
Alex Norris <Alexanderdahlawesome@gmail.com>
2017-05-10 18:55:59 +0200

    Som om det ikke helt vil som jeg vil..

--- Projekt/Beregninger/Kabinet.m
+++ Projekt/Beregninger/Kabinet.m
@@ -5,60 +5,66 @@ clc,clear,close all;
 %% Fysiske Parametre
 rho=1.18;                            %Air Mass Density (kg/m3)
 c=345;                               %Speed of sound(m/s)
-pREF=20e-6;                          %Tryk-reference
-Ug=5;
-Fresponse=40:40000;
-s=j*2*pi*Fresponse;
-
+pREF=20e-6;                          %Tryk-reference (pa)
+Ug=2.83;                             %Påtrykt Spænding (V)
+f=40:1000;                          %Frekvens (Hz)
+s=j*2*pi*f;                          %Laplace operator
+%f1=c/(2*pi*r);                      %Grænse for massestyret system
+                                     %(Herover Usikkert)
 %% Parametre FW168 MIDWOOFER
-Re=7.2;                             %DC-Modstand
-Le=Re/(2*pi*54);                    %Svingspolens Selvinduktion f3=54Hz
-Mms=014.7;                          %Masse af Bevægelige system
-Cms=0.821;                          %Eftergivelighed af styr
-fs=45; %1/(2*pi*sqrt(Mms*Cms));     %Resonansfrekvens
-Qms=3.246;  %(1/Rms)*sqrt(Mms/Cms); %Mekanisk Godhed
-Qes=0.452;                          %Elektrisk Godhed
-Rms=0;                              %Mekanisk tabsmodstand
-Vas=16.5;%L                         %Ækvivalent Volumen
-Bl=8.2;                             %Kraftfaktor
-SD=119;                             %Membranens effektive Areal
-Xmax=4.6;                           %Maksimal Lineær Bevægelse
-S=87.3;                             %Følsomhed
+Re=7.2;                                %DC-Modstand (ohm)
+f3=48;
+Le=Re/(2*pi*f3);                       %Svingspolens Selvinduktion (H)
+Mms=14.7;                           %Masse af Bevægelige system (kg)
+Cms=0.821;                          %Eftergivelighed af styr (mm/N)
+fs=45;                                 %Resonansfrekvens (Hz)
+Qms=3.246;                             %Mekanisk Godhed
+Qes=0.452;                             %Elektrisk Godhed
+Rms=(1/Qms)*sqrt(Mms/Cms);             %Mekanisk tabsmodstand (ohm)
+Vas=16.5e-3;                           %Ækvivalent Volumen (L)
+Bl=8.2;                                %Kraftfaktor (Tm)
+SD=119e-2;                             %Membranens effektive Areal (m^2)
+Xmax=4.6e-3;                           %Maksimal Lineær Bevægelse (m)
+%S=87.3;                               %Følsomhed (dB)
 
 
 %% Rum- & Måleparametre
-R=1;                                %Distance til Mikrofon
-FA=(Bl*Ug)/(Re*SD);                 %Akustisk kraft
-CAB=Vas/(rho*c^2);                   % Box compliance.
-RAE=(Bl)^2/(Re*SD^2);               % Electrical DC resistance.
-MAS=Mms/(SD^2);                     % Driver moving mass (kg).
-CAS=Cms*SD^2;                       % Driver compliance (m/N)
-RAS=Rms/SD^2;                       % Driver mechanical loss (Ns/m).
-RAL=10e4;                           % Cabinet losses (Ns/m5).
-RP=0.015;                           % Port radius (m).
-SP=pi*RP^2;                         % Port area (m2)
-LX=0.200;
+r=1;                                %Distance til Mikrofon
+Fa=(Bl*Ug)/(Re*SD);                 %Akustisk kraft
+Cab=Vas/(rho*c^2);                  % Akustik eftergivelighed af luftvoluminet
+Rae=(Bl)^2/(Re*SD^2);               % Electrical DC resistance.
+Mas=Mms/(SD^2);                     % Driver moving mass (kg).
+Cas=Cms*SD^2;                       % Driver compliance (m/N)
+Ras=Rms/SD^2;                       % Driver mechanical loss (Ns/m).
+%RAL=10e4;                          % Cabinet losses (Ns/m5).
+RP=0.05;                            % Port radius (m).
+SP=pi*RP^2;                         % Areal af Port (m2)
+LX=0.200;                           %Port længde (m)
 MMP=(rho*SP)*(LX+1.5*sqrt(SP/pi));
-MAP=MMP/SP^2; % Driver moving mass (kg) 
-qF=FA./(RAE+s*MAS+1./(s*CAS)+RAS+1./(s*CAB+1./(s*MAP)));
-qP=-qF.*(1./(s*CAB))./(1./(s*CAB)+s*MAP);
-pT=rho*s.*(qF+qP)/(2*pi*R); 
-pF=rho*s.*qF/(2*pi*R); 
-pP=rho*s.*qP/(2*pi*R);
+%MAP=MMP/SP^2;                       % Akustisk masse af luften i porten (kg) 
+MAP=(rho/SP)*(LX+1.5*sqrt(SP/pi));
+qF=Fa./(Rae+s*Mas+1./(s*Cas)+Ras+1./(s*Cab+1./(s*MAP)));
+qP=-qF.*(1./(s*Cab))./(1./(s*Cab)+s*MAP);
+pT=rho*s.*(qF+qP)/(2*pi*r); 
+pF=rho*s.*qF/(2*pi*r); 
+pP=rho*s.*qP/(2*pi*r);
 LT=20*log10(abs(pT)/pREF); 
 LD=20*log10(abs(pF)/pREF); 
 LP=20*log10(abs(pP)/pREF); 
 
-semilogx(Fresponse,LT,'-r','LineWidth',1 ...
-    Fresponse,LD,'-g','LineWidth',1, ...
-    Fresponse,LP,'-b','LineWidth',1) 
+hold on, grid on
+semilogx(f,LT,'-r','LineWidth',2)
+semilogx(f,LD,'-g','LineWidth',1)
+semilogx(f,LP,'-b','LineWidth',1);
 legend('Samlet','Enhed','Port')
-CMB=Vas/(rho*c^2*SD^2); 
-CRES=Cms*CMB/(Cms+CMB); 
+  hold off
+  
+Cmb=Vas/(rho*c^2*SD^2);
+CRES=Cms*Cmb/(Cms+Cmb); 
 FC=1/(2*pi*sqrt(Mms*CRES)); 
-FP=1/(2*pi*sqrt(MAP*CAB)); 
+FP=1/(2*pi*sqrt(MAP*Cab)); 
 QTS=sqrt(Mms/CRES)/(Bl^2/Re+Rms); 
-QP=RAE*sqrt(CAB/MAP);
+QP=Rae*sqrt(Cab/MAP);
 
 text(11,97, ['VB=' num2str(1000*Vas) 'liter'])
 text(11,95, ['FC=' num2str(round(FC)) 'Hz'])
@@ -66,6 +72,6 @@ text(11,93, ['FP=' num2str(round(FP)) 'Hz'])
 text(11,91, ['QTS=' num2str(round(100*QTS)/100)]) 
 text(11,89, ['QP=' num2str(round(100*QP)/100)]) 
 text(11,85, ['UG=' num2str(round(Ug)) ' V']) 
-text(11,83, ['R=' num2str(round(R)) ' m']) 
+text(11,83, ['r=' num2str(round(r)) ' m']) 
 text(11,79, ['RP=' num2str(RP) ' m'])
 text(11,77, ['LP=' num2str(LX) ' m'])
