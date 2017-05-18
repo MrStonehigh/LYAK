82cad20854109d27a3f66167939631d6b2a92333
Alex Norris <Alexanderdahlawesome@gmail.com>
2017-05-18 15:40:47 +0200

    Refleksionsbidrag. Fungerer ikke endnu

--- Projekt/Beregninger/Refleksion.m
+++ Projekt/Beregninger/Refleksion.m
@@ -2,50 +2,44 @@
 clc,clear,close all;
 %% Parametre FW168 MIDWOOFER
 Re=7.2;                                %DC-Modstand (ohm)
-f3=48;                                 % Hz
-Le=Re/(2*pi*f3);                       %Svingspolens Selvinduktion (H)
-Mms=14.7%e-3;                          %Masse af Bevægelige system (kg)
-Cms=0.821%e-3;                         %Eftergivelighed af styr (mm/N)
-fs=45;                                 %Resonansfrekvens (Hz)
-Qms=3.246;                             %Mekanisk Godhed
-Qes=0.452;                             %Elektrisk Godhed
-Rms=(1/Qms)*sqrt(Mms/Cms);             %Mekanisk tabsmodstand (ohm)
-Vas=16.5e-3;                           %Ækvivalent Volumen (L)
+Mms=14.7e-3;                           %Masse af Bevægelige system (kg)
 Bl=8.2;                                %Kraftfaktor (Tm)
-SD=119;%e-2;                             %Membranens effektive Areal (m^2)
-Xmax=4.6%;e-3;                           %Maksimal Lineær Bevægelse (m)
-%S=87.3;                               %Følsomhed (dB)
+SD=119e-4;                             %Membranens effektive Areal (m^2)
+Xmax=4.6e-3;                           %Maksimal Lineær Bevægelse (m)
 
 
 %% Fysiske Parametre
 
 Ug=2.75;                             %Påtrykt spænding (V)
 rho=1.18;                            %Air Mass Density (kg/m3)
-f=1:10000;                           %Frequency (Hz)
+f=0:1000;                            %Frequency (Hz)
 pRef=20e-6;                          %Referencetryk (pa)
 rd=1;                                %Distance to Microphone (m)
 c=343;                               %Lydens hastighed (m/s)
-D=1;                                 %Spredning (approximeret)
-k=(2*pi*f)/c;                        %Bølgetallet
+h=.20;                               %Højtalerens højde ift gulv (m)
 
 
-%% Portens Parametre
+%% Retningsvirkning
 
-r=1;                                  %Måleafstand (m)
-h=.20;                                %Højtalerens højde ift gulv (m)
-rp=0.025;                             %Port Radius (m)
-SP=pi*rp^2;                           %Portens effektive Areal (m^2)
-pRMS=((rho*SP*(2*pi*f).^2)/(4*pi*r)); %xmax/sqrt(2);
-Lp=.15;                               %Længden på porten (m);
-Map=(rho/SP)*(Lp+1.5*sqrt(SP/pi));    %Massen ved porten
+a=sqrt(SD/pi);                       %Enhedsradius (m)
+ka=(2*pi*f.*a)/c;                    %Spredningsfaktor
+rR=sqrt(rd^2+4*h^2);                 %Reflekteret lydafstand (m)
+v=(Bl*Ug)./(2*pi*f*Mms*Re);          %Membranens hastighed (m/s)
+q=SD*v;                              %Volumenhastighed (m3/s)
+sinteta=(2*h)/rR;                    %sin(teta)
+
+J=zeros(length(f));
+for k=1:f(end)+1
+    J(k)=sum((((-1)^k)/(factorial(k)*factorial(1+k)))*((ka*sinteta)/2).^(2*k+1));
+end
+J=sum(J(:));
+D=(2*J)./(ka*sinteta);             %Spredning 
 
 %% Beregninger 
-v=(Bl)./(2*pi*f.*Map);          %Volumenhastighed
-q=SP*v;
-rR=sqrt(rd^2+4*h^2);                 %Reflekteret lydafstand (m)
+k=2*pi*f./c;                                    %Bølgetallet
 
-fR=c/(2*(rR-rd));                    %Portens Resonansfrekvens (Hz)
-%p=j.*((rho.*f.*q)/(2*rd)).*...
-%    (1+(rd/rR).*D*exp(-j*k*(rR-rd)));%Trykbidrag fra overfladerefleksion
-p=j*((rho*f)/r).*q;
-semilogx(f,(p))
+pR=j.*((rho*f.*q)/(2*rd)).*(1+(rd/rR)*D.*exp(-j*k*(rR-rd)));
+LR=20*log10(abs(pR)/pRef);
+
+figure,
+semilogx(LR)
