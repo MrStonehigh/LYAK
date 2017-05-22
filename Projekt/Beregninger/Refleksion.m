74e5b879c796438cc4cb3bf3be8c0ea1552db8fb
Alex Norris <Alexanderdahlawesome@gmail.com>
2017-05-22 13:06:38 +0200

    Fuck tores elendige bog, med elendige formler, der giver elendige resultater..

--- Projekt/Beregninger/Refleksion.m
+++ Projekt/Beregninger/Refleksion.m
@@ -1,45 +1,59 @@
 %% Refleksion
 clc,clear,close all;
 %% Parametre FW168 MIDWOOFER
-Re=7.2;                                %DC-Modstand (ohm)
-Mms=14.7e-3;                           %Masse af Bevægelige system (kg)
-Bl=8.2;                                %Kraftfaktor (Tm)
-SD=119e-4;                             %Membranens effektive Areal (m^2)
-Xmax=4.6e-3;                           %Maksimal Lineær Bevægelse (m)
+Re=7.2;                              %DC-Modstand (ohm)
+Mms=14.7e-3;                         %Masse af Bevægelige system (kg)
+Bl=8.2;                              %Kraftfaktor (Tm)
+SD=119e-4;                           %Membranens effektive Areal (m^2)
+Cms=0.821e-3;                           %Eftergivelighed af styr (m/N)
+Qms=3.246;                              %Mekanisk Godhed
+Rms=sqrt(Mms/Cms)/Qms;                  %Mekanisk tabsmodstand (ohm)
 
 
 %% Fysiske Parametre
 
 Ug=2.75;                             %Påtrykt spænding (V)
 rho=1.18;                            %Air Mass Density (kg/m3)
-f=0:1000;                            %Frequency (Hz)
+f=1:10000;                           %Frequency (Hz)
 pRef=20e-6;                          %Referencetryk (pa)
 rd=1;                                %Distance to Microphone (m)
-c=343;                               %Lydens hastighed (m/s)
+c=345;                               %Lydens hastighed (m/s)
 h=.20;                               %Højtalerens højde ift gulv (m)
+s=j*2*pi*f;                          %Laplace operator
+k=2*pi*f./c;                         %Bølgetallet
 
 
 %% Retningsvirkning
 
-a=sqrt(SD/pi);                       %Enhedsradius (m)
-ka=(2*pi*f.*a)/c;                    %Spredningsfaktor
-rR=sqrt(rd^2+4*h^2);                 %Reflekteret lydafstand (m)
-v=(Bl*Ug)./(2*pi*f*Mms*Re);          %Membranens hastighed (m/s)
-q=SD*v;                              %Volumenhastighed (m3/s)
-sinteta=(2*h)/rR;                    %sin(teta)
+a=sqrt(SD/pi);                       %Enhedsradius (m) 
+ka=(2*pi*f*a)./c;                    %Spredningsfaktor 
+rR=sqrt((rd^2)+4*(h^2));             %Reflekteret lydafstand (m) 
+v=(Bl*Ug)./(2*pi*f*Mms*Re);          %Membranens hastighed (m/s) ?? falder?!
+%v=((Bl/Mms)*s)./...
+%    ((s.^2)+(((Bl)^2)/(Mms*Re)+Rms/Mms)*s+1/(Mms*Cms))*Ug/Re;
+q=SD*v;                              %Volumenhastighed (m3/s) 
+sinteta=(2*h)/rR;                    %sin(teta) 
+teta=asin(sinteta)
 
-J=zeros(length(f));
-for k=1:f(end)+1
-    J(k)=sum((((-1)^k)/(factorial(k)*factorial(1+k)))*((ka*sinteta)/2).^(2*k+1));
+%{
+J=0;
+for m=1:50
+    J=J+(((-1)^m)/(factorial(m)*factorial(m+1))).*(((((teta))/2)).^(2*m+1));
 end
-J=sum(J(:));
-D=(2*J)./(ka*sinteta);             %Spredning 
+%J=sum(J)
+%}
+J=besselj(1,ka*sinteta);              %Besselfunktionen
+D=((2*J)./(ka*sinteta));             %Direktivitet 
 
 %% Beregninger 
-k=2*pi*f./c;                                    %Bølgetallet
+%AFSNIT OM REFLEKSION
+pD=j.*(rho*f.*q)/(2*rd).*exp(-j*k*rd);
+%pR=j*((rho*f.*q)/(2*rR)).*exp(-j*k*rR);
+%p=pD+pR;
+p=j*((rho*f.*q)./(2*rd)).*(1+(rd/rR).*(D).*exp(-j*k*(rR-rd)));
 
-pR=j.*((rho*f.*q)/(2*rd)).*(1+(rd/rR)*D.*exp(-j*k*(rR-rd)));
-LR=20*log10(abs(pR)/pRef);
-
+LR=20*log10(abs(p)/pRef);
+%LR=mag2db(abs(p));
 figure,
 semilogx(LR)
+axis([0 f(end) min(LR) max(LR)])
