96d0c2d3aa1373796b3359a08842b3b9a600231a
Alex Norris <Alexanderdahlawesome@gmail.com>
2017-05-23 12:45:32 +0200

    Ready for tests

--- Projekt/Beregninger/Refleksion.m
+++ Projekt/Beregninger/Refleksion.m
@@ -1,5 +1,11 @@
 %% Refleksion
 clc,clear,close all;
+
+%% Ændring af Måleparametre
+
+rd=1;                                %Distance to Microphone (m)
+h=.2;                                %Højtalerens højde ift gulv (m)
+
 %% Parametre FW168 MIDWOOFER
 Re=7.2;                              %DC-Modstand (ohm)
 Mms=14.7e-3;                         %Masse af Bevægelige system (kg)
@@ -16,11 +22,9 @@ Ug=2.75;                             %Påtrykt spænding (V)
 rho=1.18;                            %Air Mass Density (kg/m3)
 f=1:10000;                           %Frequency (Hz)
 pRef=20e-6;                          %Referencetryk (pa)
-rd=1;                                %Distance to Microphone (m)
 c=345;                               %Lydens hastighed (m/s)
-h=1;                                %Højtalerens højde ift gulv (m)
 s=j*2*pi*f;                          %Laplace operator
-k=2*pi*f/c;                         %Bølgetallet
+k=2*pi*f/c;                          %Bølgetallet
 
 
 %% Retningsvirkning
@@ -28,41 +32,42 @@ k=2*pi*f/c;                         %Bølgetallet
 a=sqrt(SD/pi);                       %Enhedsradius (m) 
 ka=(2*pi*f*a)./c;                    %Spredningsfaktor 
 rR=sqrt((rd^2)+4*(h^2));             %Reflekteret lydafstand (m) 
-%v=(Bl*Ug)./(2*pi*f*Mms*Re);          %Membranens hastighed (m/s) ?? falder?!
+           
 v=abs(((Bl/Mms)*s)./...
-    ((s.^2)+(((Bl)^2)/(Mms*Re)+Rms/Mms)*s+1/(Mms*Cms))*Ug/Re);
+((s.^2)+(((Bl)^2)/(Mms*Re)+Rms/Mms)...
+    *s+1/(Mms*Cms))*Ug/Re);          %Membranens hastighed (m/s)
+
 q=SD*v;                              %Volumenhastighed (m3/s) 
 sinteta=(2*h)/rR;                    %sin(teta) 
-teta=asin(sinteta)
 
-%{
-J=0;
-for m=1:50
-    J=J+(((-1)^m)/(factorial(m)*factorial(m+1))).*(((((teta))/2)).^(2*m+1));
-end
-%J=sum(J)
-%}
-J=besselj(1,ka*sinteta);              %Besselfunktionen 
-%J=sum(J)
+J=besselj(1,ka*sinteta);             %Besselfunktionen 
 D=((2*J)./(ka*sinteta));             %Direktivitet 
 
 %% Beregninger 
 %AFSNIT OM REFLEKSION
-pD=j.*(rho*f.*q)/(2*rd).*exp(-j*k*rd);
-pR=j*((rho*f.*q)/(2*rR)).*exp(-j*k*rR);
-p=pD+pR;
+pD=j.*(rho*f.*q)/(2*rd).*exp(-j*k*rd);          %Direkte lydtryk (pa)
+pR=j*((rho*f.*q)/(2*rR)).*exp(-j*k*rR);         %Reflekteret bidrag (pa)
+%p=pD+pR;                                       %Samlet, u. direktivitet
 
-p=j*((rho*f.*q)/(2*rd)).*(1+(rd/rR)*(D).*exp(-j*k*(rR-rd)));
+p=j*((rho*f.*q)/(2*rd)).*(1+(rd/rR)*(D)...
+    .*exp(-j*k*(rR-rd)));                       %Samlet Lydtryk (pa)
 
-LD=20*log10(abs(pD)/pRef);
-LR=20*log10(abs(pR)/pRef);
-L=20*log10(abs(p)/pRef);%-87.3/rd;
+LD=20*log10(abs(pD)/pRef);                      %Direkte (dB)
+LR=20*log10(abs(pR)/pRef);                      %Reflekteret (dB) 
+L=20*log10(abs(p)/pRef);                        %Samlet (dB)
 
-%LR=mag2db(abs(p));
+
 figure,
-semilogx(L);hold on
-grid on
+semilogx(L); title('Lydtryk fra højtalerenhed')
+hold on, grid on
 semilogx(LD);
 semilogx(LR);
 axis([10 f(end) min(L) max(L)])
 legend('Samlet','Direkte','Reflekteret');
+xlabel('Frekvens (Hz)');
+ylabel('SPL (dB)');
+
+figure, 
+semilogx(L-LD); title('Refleksionsbidrag')
+xlabel('Frekvens (Hz)');
+ylabel('SPL (dB)');
